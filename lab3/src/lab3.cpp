9e0ef3534210e6a557f996c9b2c04677e2024ba0
Ivanko5417 <v.drobysh2011@gmail.com>
2017-09-16 20:24:06 +0300

    complete lab3

---
+++ lab3/src/lab3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class String
+{
+	char * str;	//строка
+	int length;	//длина строки
+	public:
+		String() {
+			str = new char[100];
+			length = 0;
+		}
+
+		String(char* str) {
+			Init(str);
+		}
+
+		String(const String& string) {
+			Init(string.str);
+		}
+
+		~String() {
+			delete str;
+		}
+	public:
+		void Init(char* str) {
+			this->str = new char[100];
+			strcpy(this->str, str);
+			length = strlen(this->str);
+		}
+
+		void Concat(char* str) {
+			strcat(this->str, str);
+			this->length += strlen(str);
+		}
+
+		int Length() {
+			return this->length;
+		}
+
+		char Char(int i) {
+			return this->str[i];
+		}
+
+		friend void Print(String* a) {
+			cout << a->str << endl;
+		}
+
+
+};
+
+int main() {
+	String* string = new String;
+	string->Init("111");
+	string->Concat("s");
+	Print(string);
+	cout << string->Char(2) << " " << string->Length() << endl;
+	delete string;
+	return 0;
+}
